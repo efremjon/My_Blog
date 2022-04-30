6ff051133d60ab3675dce3e482ad71aea17f3d61
Efrem Yohanis <102489707+efremjon@users.noreply.github.com>
2022-04-30 12:37:46 -0700

    part 31

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserRegistrationView,UpdateProfile,ChagePassword
+from .views import UserRegistrationView,UpdateProfile,ChagePassword,userprofile
 from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('register/',UserRegistrationView.as_view(),name="register"),
     path('edit_Profile/',UpdateProfile.as_view(),name="edit_profile"),
+    path('<int:pk>/user_profile',userprofile.as_view(),name="user_profile"),
     path('Chage_passwordw/',ChagePassword.as_view(),name='Chage_password'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,9 +1,13 @@
+from pyexpat import model
 from django.shortcuts import render,reverse
 from django.views.generic import CreateView,UpdateView
 from django.contrib.auth.views import PasswordChangeView
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm,PasswordChangeForm
 from django.urls import reverse_lazy
 from .form import signupForm,Edit_Profile
+from django.views.generic import DetailView
+from myblog.models import profile
+from django.shortcuts import get_object_or_404, render
 
 
 class UserRegistrationView(CreateView):
@@ -26,3 +30,12 @@ class ChagePassword(PasswordChangeView):
     template_name = 'registration/chage_password'
     def get_success_url(self):
         return reverse('home')
+class userprofile(DetailView):
+    model = profile
+    template_name = 'registration/profile_page.html'
+    def get_context_data(self, *args, **kwargs):
+        context=super(userprofile,self).get_context_data(*args, **kwargs)
+        user_profile=get_object_or_404(profile,id=self.kwargs['pk'])
+        context["user_profile"]=user_profile
+        
+        return context
