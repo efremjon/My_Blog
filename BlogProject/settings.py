078de39b6adb8bd2f763d37fc11b649143d023a9
Efrem Yohanis <102489707+efremjon@users.noreply.github.com>
2022-04-29 23:00:02 -0700

    part 30

--- BlogProject/settings.py
+++ BlogProject/settings.py
@@ -112,6 +112,7 @@ STATIC_URL = 'static/'
 MEDIA_URL= '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 
+STATIC_ROOT = os.path.join(BASE_DIR,'asset')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
